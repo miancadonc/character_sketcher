c527d743964ebe38ef0c9987b00cf7053038b13e
Nat McConnaughay <natmcconnaughay@gmail.com>
2021-04-03 12:27:21 -0500

    implement simple index and show actions for character controller

--- backend/character-sketcher-api/app/controllers/characters_controller.rb
+++ backend/character-sketcher-api/app/controllers/characters_controller.rb
@@ -1,2 +1,12 @@
 class CharactersController < ApplicationController
+
+    def index
+        characters = Character.all
+        render json: CharacterSerializer.new(characters).to_serialized_json
+    end
+
+    def show
+        character = Character.find(params[:id])
+        render json: CharacterSerializer.new(character).to_serialized_json
+    end
 end
