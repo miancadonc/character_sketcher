1e649ff645a7d1dbe10364a45f18dd336bb8dcd6
Nat McConnaughay <natmcconnaughay@gmail.com>
2021-04-03 12:04:41 -0500

    migrate characters, seed database for testing

--- backend/character-sketcher-api/db/seeds.rb
+++ backend/character-sketcher-api/db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+# char_1 = Character.create(name: "Gon", age: "12", gender: "Male", description: "Black spiky shounen protag hair, green shorts and jacket, and a fishing pole.", goals: "Hang out with best friend Killua. Meet dad Ging.", backstory: "Grew up on whale island as a feral nature child and favorite of the townsfolk. His aunt raised him and he's never met his parents.")
+# char_2 = Character.create(name: "Killua", age: "12", gender: "Male", description: "Little white haired cat boy with the best 'fits.", goals: "Hang out with best friend Gon. Maybe become his boyfriend. Unclear.", backstory: "His family is a famous group of assassins, and he's been trained (abused) since he was little.")
