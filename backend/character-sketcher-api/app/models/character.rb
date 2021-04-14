06ca4dcb7a31adbab8ba18cdacf1fe0e55263f1f
Nat McConnaughay <natmcconnaughay@gmail.com>
2021-04-14 12:44:16 -0500

    generate join model for chars and envs; add metaprogramming to momdels to link them

--- backend/character-sketcher-api/app/models/character.rb
+++ backend/character-sketcher-api/app/models/character.rb
@@ -1,2 +1,4 @@
 class Character < ApplicationRecord
+    has_many :environment_characters
+    has_many :environments, through: :environment_characters
 end
--- backend/character-sketcher-api/app/models/environment.rb
+++ backend/character-sketcher-api/app/models/environment.rb
@@ -1,2 +1,4 @@
 class Environment < ApplicationRecord
+    has_many :environment_characters
+    has_many :characters, through: :environment_characters
 end
