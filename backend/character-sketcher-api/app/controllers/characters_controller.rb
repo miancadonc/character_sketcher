43f639b10650e565535a02143470447c56e1a45d
Nat McConnaughay <natmcconnaughay@gmail.com>
2021-04-16 01:01:41 -0500

    build characters_controller#create action

--- backend/character-sketcher-api/app/controllers/characters_controller.rb
+++ backend/character-sketcher-api/app/controllers/characters_controller.rb
@@ -9,4 +9,15 @@ class CharactersController < ApplicationController
         character = Character.find(params[:id])
         render json: CharacterSerializer.new(character).to_serialized_json
     end
+
+    def create
+        character = Character.new(name: params[:name], age: params[:age], gender: params[:gender], description: params[:description], goals: params[:goals], backstory: params[:backstory])
+        if params[:id] > 1
+            Environment.find(params[:id]).characters << character
+        end
+
+        Environment.find(1).characters << character
+
+        character.save
+    end
 end
